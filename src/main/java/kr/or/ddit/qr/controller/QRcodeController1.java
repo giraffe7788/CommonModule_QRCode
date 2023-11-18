b0eeda9271b56f4a1fbfbf6496f90cb63f9a9943
PC-23 <PC-23@DESKTOP-4OSK6AA>
2023-11-18 17:53:16 +0900

    1118 qr2번쨰 진행중

--- src/main/java/kr/or/ddit/qr/controller/QRcodeController1.java
+++ src/main/java/kr/or/ddit/qr/controller/QRcodeController1.java
@@ -6,6 +6,10 @@ import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -20,10 +24,12 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Base64;
 
 /**
  * 첫번째 QR 로그인 방법 QR코드 링크 생성하기
  */
+@Slf4j
 @Controller
 @RequestMapping("/qr1")
 public class QRcodeController1 {
@@ -33,35 +39,67 @@ public class QRcodeController1 {
 		return "qr1/createQRcodeLink";
 	}
 
+//	@PostMapping("/getQRcode")
+//	public ResponseEntity<byte[]> qrToTistory(@RequestParam String url) throws WriterException, IOException {
+//		
+//		int width = 200;
+//		int height = 200;
+//
+//		BitMatrix encode = new MultiFormatWriter().encode(url, BarcodeFormat.QR_CODE, width, height);
+//
+//		// 파일 저장을 위한 객체 생성 
+//		File file = new File("qr저장링크");
+//		FileOutputStream fileOutputStream = new FileOutputStream(file);
+//
+//		try {
+//			ByteArrayOutputStream out = new ByteArrayOutputStream();
+//
+//			MatrixToImageWriter.writeToStream(encode, "PNG", out);
+//
+////			byte[] imageBytes = out.toByteArray();
+////
+////		    HttpHeaders headers = new HttpHeaders();
+////		    headers.setContentType(MediaType.IMAGE_PNG);
+//
+////		    return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
+//
+//			
+//			// 파일 저장부분 
+//			// fileOutputStream.write(out.toByteArray());
+//			return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(out.toByteArray());
+//
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		} finally {
+//			fileOutputStream.close();
+//		}
+//
+//		return null;
+//	}
+	
 	@ResponseBody
-	@PostMapping("/getQRcode")
+	@PostMapping(value="/getQRcode", produces="text/plain;charset=UTF-8")
 	public ResponseEntity<byte[]> qrToTistory(@RequestParam String url) throws WriterException, IOException {
-		int width = 200;
-		int height = 200;
+		
+		log.info("url : " + url);
+		
+	    int width = 200;
+	    int height = 200;
+	    
+	    // qr생성부분
+	    BitMatrix encode = new MultiFormatWriter().encode(url, BarcodeFormat.QR_CODE, width, height);
+	    ByteArrayOutputStream out = new ByteArrayOutputStream();
+	    MatrixToImageWriter.writeToStream(encode, "PNG", out);
 
-		BitMatrix encode = new MultiFormatWriter().encode(url, BarcodeFormat.QR_CODE, width, height);
+	    // out을 Base64로 인코딩 후 반환
+	    byte[] imageBytes = out.toByteArray();
+	    String base64Encoded = Base64.getEncoder().encodeToString(imageBytes);
+	    log.info(base64Encoded);
+	    
+	    HttpHeaders headers = new HttpHeaders();
+	    headers.setContentType(MediaType.IMAGE_PNG);
 
-		// 파일 저장을 위한 객체 생성 
-		File file = new File("qr저장링크");
-		FileOutputStream fileOutputStream = new FileOutputStream(file);
-
-		try {
-			ByteArrayOutputStream out = new ByteArrayOutputStream();
-
-			MatrixToImageWriter.writeToStream(encode, "PNG", out);
-
-			// 파일 저장부분 
-			// fileOutputStream.write(out.toByteArray());
-
-			return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(out.toByteArray());
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			fileOutputStream.close();
-		}
-
-		return null;
+	    return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
 	}
 
 }
--- src/main/webapp/WEB-INF/views/qr1/createQRcodeLink.jsp
+++ src/main/webapp/WEB-INF/views/qr1/createQRcodeLink.jsp
@@ -13,6 +13,7 @@
 	<button type="button" id="submitBtn">제출</button>
 	<br><br>
 	<button type="button" id="btnNaver">네이버 링크 QR코드 생성</button>
+	<img id="qrCodeImage" alt="QR Code" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMgAAADIAQAAAACFI5MzAAABIUlEQVR42u2YTRKDIAyF44pjeFORm3IMVqTJi1Rq7bbJQgYd5GORyc8LI/GvQQ95yJ/JTjLWwnVpa2n6sYQhRZ627qRTFsdOENJI7OUG23EkHll7VCIx3+DRWATRlkzckpz6zgNPclRJs3lTP47Ehvm13OiOJ5HwditfrjC5LmFITzqVJyVbCkQOPeaasehzlXgT1WOxNzHET6yeo+1MzOqujqz5UtveBC5Uq3dEO0+Z6E5kIzcSe7H4iLY7gSSzelS3ic5o+5NdKhitTLehLjkMGaosyag5WM5M9CfWzbrmoPJQ5H3rRBFzOT0agKDHartI1s0uN2J/wsOX8pmDkTKcSlOn9ScmMONevPDUzbyJVQk0Bu90qR9P8vw/eIg/eQGpCd2saZ+EZAAAAABJRU5ErkJggg==" />
 </form>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script>
@@ -24,18 +25,24 @@ $(function(){
 		};
 		
 		$.ajax({
-			url : '/qr1/getQRcode',
-			type : 'post',
-			data : data,
-			dataType : 'json',
-			success : function(rst){
-				console.log(rst);
-			},
-			error : function(rst){
-				console.log('통신 실패');
-				console.log(rst);
-			}
-		});
+	        url: '/qr1/getQRcode',
+	        method: 'POST',
+	        data: data, // 실제 사용하려는 URL로 변경
+	        dataType: 'json',
+	        success: function (data) {
+	        	console.log('성공');
+	        	console.log(data);
+	        	
+	        	$('#qrCodeImage').attr('src', 'data:image/png;base64,');
+// 	        	img.src = 'data:image/png;base64,' + base64String;
+	        	
+	        },
+	        error: function (error) {
+	            console.error('실패');
+	            console.log(error);
+	        }
+	    });
+		
 	});
 	
 	$('#btnNaver').on('click',function(){
